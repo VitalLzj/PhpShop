eb5df3947e73424929fb4a5ee558e830183f3264
VitalLzj <976623696@qq.com>
2017-03-14 11:04:03 +0800

    测试提交

---
+++ shop/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/3/10 0010
+ * Time: 上午 8:49
+ */
+
+define('APP_DEBUG', true);   //开发调试模式
+
+//接入tp
+include ("../ThinkPHP/ThinkPHP.php");
